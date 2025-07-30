b3058632229bff4c268006b0ee2f1f2e4fd9462d
Jin Kwon <onacit@gmail.com>
2025-07-30 21:28:17 +0900

    wip

--- resources/mysql_9_3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#cat ./_mysql.txt | awk '{ print "new Word(\""$1"\", true,"} {print $2=(R)? "true":"false"")," }'
-#cat ./mysql_9.3_keywords_and_reserved_words.txt | awk '{ print $1, "true", $2=(R)? "true" : "false" }'
-cat ./mysql_9_3.txt | awk '{ print $1 }'
-
